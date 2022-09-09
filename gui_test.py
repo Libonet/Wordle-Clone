1cd7892243b920f598afd5c685f67cc071b80021
Santiago Libonati <santiago.libonati@gmail.com>
2022-09-08 22:59:19 -0300

    Cambios al input, visuales, y agregado font

--- gui_test.py
+++ gui_test.py
@@ -1,63 +1,82 @@
+import pyglet
+from pyglet.libs.win32 import constants
 import PySimpleGUI as sg
-from pynput import keyboard
 
-def keyPress(key, cache, count):
-    if hasattr(key,'char') and key.char!=None and count[0]<5:
-        if key.char.isalpha():
-            cache[count[0]]=key.char
-            count[0] = count[0] + 1
-    if key == keyboard.Key.backspace and count[0]>0:    
-        cache[count[0]-1]=""
-        count[0] = count[0] - 1
-    print(cache)
+constants.COINIT_MULTITHREADED = 0x2  # 0x2 = COINIT_APARTMENTTHREADED
+
+pyglet.font.add_file(r"./clear-sans-main/TTF/ClearSans-Regular.ttf")
+
+sg.theme("Default1")
+gray = "#818384"
+darkGray = "#1f1f1f"
+
+def getInputGUI(value, coordinates):
+    inputBox = sg.Input(
+        value,
+        key=coordinates,
+        size=(2,1),
+        background_color=darkGray,
+        pad=2,
+        text_color="white",
+        justification="centered",
+        font=("Clear sans", 35),
+        disabled=True,
+        disabled_readonly_background_color=darkGray,
+        enable_events=True,
+    )
+    return inputBox
+
+def getButtonGUI(char):
+    buttonBox = sg.Button(
+        char,
+        key=char,
+        size=(2,1),
+        font=("Clear sans", 20),
+        pad=2,
+        button_color=("white", gray),
+        
+    )
+
+    return buttonBox
 
 def main():
-    cache = ["","","","",""]
-    count = [0]
-    def on_press(key):
-        keyPress(key, cache, count)
-
-    """ def on_release(key):
-        print('{0} released'.format(
-            key))
-        if key == keyboard.Key.esc:
-            # Stop listener
-            return False """
-
-    # Collect events until released
-    #with keyboard.Listener(on_press=on_press) as listener:
-    #    listener.join()
-
-    # ...or, in a non-blocking fashion:
-    listener = keyboard.Listener(
-        on_press=on_press)
-    listener.start()
-
     # Define the window's contents
     layout = [
-            # esto debería ser un output de las letras que capturo con el listener [],
-            [sg.Input(size=(20, 1), key='-INPUT-')],
-            [sg.Text(size=(40,1), key='-OUTPUT-')],
-            [sg.Button('Ok'), sg.Button('Quit')]]
+        [[getInputGUI("", (row, column)) for column in range(5)] for row in range(6)], # las casillas del input del juego
+        [sg.Text(background_color="#1f1f1f")],
+        [sg.Text(background_color="#1f1f1f")],
+        [sg.Text(background_color="#1f1f1f")],
+        [getButtonGUI(char) for char in "QWERTYUIOP"],
+        [getButtonGUI(char) for char in "ASDFGHJKL"],
+        [getButtonGUI(char) for char in "ZXCVBNM"],
+    ]
 
     # Create the window
-    window = sg.Window('Wordle Game Thing', layout)
+    window = sg.Window(
+        'Wordle clone',
+        layout, 
+        margins=(10, 10), 
+        background_color="#1f1f1f", 
+        element_justification="center", 
+        finalize=True,)
+
+    [window[(0, 0)].update(disabled=False)]
+    [window[(row, col)].set_cursor(cursor_color=darkGray) for col in range(5) for row in range(6)]
 
     while True:
         # Display and interact with the Window using an Event Loop
         event, values = window.read()
-        # while True:
+
             
         # See if user wants to quit or window was closed
-        if event == sg.WINDOW_CLOSED or event == 'Quit':
+        if event == sg.WINDOW_CLOSED:
             break
-        # Output a message to the window
-        if event == 'Ok':
-            window['-OUTPUT-'].update('Hello ' + values['-INPUT-'] + "! Thanks for trying PySimpleGUI")
 
-        # Finish up by removing from the screen
+        # Output a message to the window
+        # window["key"].update("What you want in there")
+
+    # Finish up by removing from the screen
     window.close()
-    listener.stop()
 
 if __name__ == "__main__":
     main()
