071ad251ce926e9ce524aab3798ffbee0ea24d2a
Santiago Libonati <santiago.libonati@gmail.com>
2022-09-11 01:37:03 -0300

    Input working correctly

--- main.py
+++ main.py
@@ -56,6 +56,25 @@ def getNextFocus(actualFocus):
 
 # -----------------------------------------------------------
 
+def updateInput(actualFocus, event, values, guess):
+    focus = False
+    # if the event is an input, it will also be a tuple
+    if isinstance(event, tuple):
+        if not values[event].isalpha():
+            actualFocus.update(value="")
+        else:
+            focus = True
+            actualFocus.update(value=values[event].upper())
+            guess = guess + values[event].upper()
+    
+    # if the event is a button, it will be the letter it displays
+    if isinstance(event, str) and event in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
+        focus = True
+        actualFocus.update(value=event)
+        guess = guess + event
+
+    return (guess, focus)
+
 # all the code of the layout and the game logic is in here
 def main():
     # Setting up the layout:
@@ -99,16 +118,16 @@ def main():
         element_justification="center", 
         finalize=True,)
     window.bind("<BackSpace>", "-BACKSPACE-")
+    window.bind("<Return>", "-ENTER-")
 
     window["-PRINT-"].update(visible=False) # this is done to get the proportions right
-    actualFocus = window[(0,0)]
+    actualFocus = window[(0,0)] # actualFocus follows the input element that the user writes in
     actualFocus.update(disabled=False)
     [window[(row, col)].set_cursor(cursor_color=darkGray) for col in range(5) for row in range(6)]
 
 # -----------------------------------------------------------
 
     # Variables for game logic
-    letterCount = 0
     win = False
     wordList = []
     with open("words.txt", mode='rt') as f:
@@ -122,6 +141,8 @@ def main():
     # Display and interact with the Window using an Event Loop
     while True:
         event, values = window.read() # getting the events
+        print(event)
+
         # See if user wants to quit or window was closed
         if event == sg.WINDOW_CLOSED:
             break
@@ -129,42 +150,51 @@ def main():
         # on a new event, delete the warning
         window["-PRINT-"].update(visible=False)
 
-        # if the event is an input, it will also be a tuple
-        if isinstance(event, tuple) and letterCount < 5:
-            if not values[event].isalpha():
-                actualFocus.update(value="")
-            else:
-                letterCount+=1
-                actualFocus.update(value=values[event].upper())
-                actualFocus = getNextFocus(actualFocus)
-                guess = guess + values[event].upper()
-                
-
         # if event is a backspace, go to the previous focus and delete the character
-        elif event == "-BACKSPACE-" and letterCount > 0:
-            letterCount-=1
-            actualFocus.update(disabled=True)
+        if event == "-BACKSPACE-" and len(guess) > 0 and len(guess) <= 4:
+            actualFocus.update(disabled=True) # disable this input box then
             actualFocus = actualFocus.get_previous_focus() # Go to last focus
             actualFocus.update(disabled=False)
-            actualFocus.update(value="")
             actualFocus.set_focus()
+            actualFocus.update(value="")
             guess = guess[:-1]
+            continue
 
-        # if event is an enter, check the word
-        elif event == "-ENTER-":
-            if letterCount!=5:
+        # if it's still possible to add letters to row
+        if len(guess) < 4:
+            if event == "-ENTER-":
                 window["-PRINT-"].update("Not enough letters", visible=True)
-            elif guess not in wordList:
-                window["-PRINT-"].update("Word not in list", visible=True)
 
-        # if the event is a button, it will be the letter it displays
-        elif event in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
-            letterCount+=1
-            actualFocus.update(value=event)
-            actualFocus = getNextFocus(actualFocus)
-            guess = guess + event
+            guess, focus = updateInput(actualFocus, event, values, guess)
+            if focus:
+                actualFocus = getNextFocus(actualFocus)
+            continue
 
+        # if it's possible to add a single letter to the row, we don't change the focus
+        if len(guess) == 4:
+            if event == "-ENTER-":
+                window["-PRINT-"].update("Not enough letters", visible=True)
 
+            guess, focus = updateInput(actualFocus, event, values, guess)
+            actualFocus.update(disabled=True)
+            continue
+
+        # if it's not possible to add letters to row
+        if len(guess) == 5:
+            # if event is a backspace, delete the character
+            if event == "-BACKSPACE-" and len(guess) > 0:
+                actualFocus.update(disabled=False)
+                actualFocus.update(value="")
+                guess = guess[:-1]
+
+            # if event is an enter, check the word
+            if event == "-ENTER-":
+                if guess not in wordList:
+                    window["-PRINT-"].update("Word not in list", visible=True)
+                # else:
+                    # checkColor(guess)
+            continue
+                
     # Finish up by removing from the screen
     window.close()
 
